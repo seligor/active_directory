bce00d981e6b30798ee5f2bb7931cdd7b6f10a26
Igor Baluev <s-el-igor@mail.ru>
2015-12-18 10:08:19 +0500

    Update diskutil.ps1

--- diskutil.ps1
+++ diskutil.ps1
@@ -1,16 +1,19 @@
 Function Get-Diskutil {
           Param([string] $computername=$env:computername) 
           Process { 
-                if ($) {$computername=$} 
+                if ($_) {$computername=$_} 
                 gwmi win32_logicaldisk -fi "drivetype=3" -comp $computername | 
-                  Select @{Name="Computername";Expression={$.SystemName}}, 
+                  Select @{Name="Computername";Expression={$_.SystemName}}, 
                   DeviceID, 
-                  @{Name="SizeGB";Expression={"{0:N2}" -f ($.Size/1GB)}}, 
-                  @{Name="FreeGB";Expression={"{0:N2}" -f ($.Freespace/1GB)}}, 
-                  @{Name="UsedGB";Expression={"{0:N2}" -f (($.Size-$.Freespace)/1GB)}}, 
-                  @{Name="PerFree";Expression={"{0:P2}" -f ($.Freespace/$.Size)}} 
+                  @{Name="SizeGB";Expression={"{0:N2}" -f ($_.Size/1GB)}}, 
+                  @{Name="FreeGB";Expression={"{0:N2}" -f ($_.Freespace/1GB)}}, 
+                  @{Name="UsedGB";Expression={"{0:N2}" -f (($_.Size-$_.Freespace)/1GB)}}, 
+                  @{Name="PerFree";Expression={"{0:P2}" -f ($_.Freespace/$_.Size)}} 
                   } 
             } 
  #Get-Diskutil urc0504 | ft -auto 
- $data=gc c:\scr\powershell\servers.txt | where {Test-Connection $ -quiet -count 2} | Get-Diskutil | Sort Computername | ConvertTo-Html -Title "Utilization" | Out-File "c:\scr\powershell\html.html" 
- $data=gc c:\scr\powershell\servers.txt | where {Test-Connection $_ -quiet -count 2} | Get-Diskutil | Sort Computername | Export-Csv c:\scr\powershell\file.csv
+ $cfg_file = 'c:\scr\powershell\servers.txt'
+ if (Test-Path $cfg_file){
+ $data=gc $cfg_file | where {Test-Connection $_ -quiet -count 2} | Get-Diskutil | Sort Computername | ConvertTo-Html -Title "Utilization" | Out-File "c:\scr\powershell\html.html" 
+ $data=gc $cfg_file | where {Test-Connection $_ -quiet -count 2} | Get-Diskutil | Sort Computername | Export-Csv c:\scr\powershell\file.csv
+ } else { 'please input names of servers into file ',$cfg_file } 
